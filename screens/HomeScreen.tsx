e49a2b4332f80206fd5c80f1ceb35aa1658c74a6
­ ­ <thiagoantonio.neves@usp.br>
2024-05-08 16:14:10 -0300

    Teste

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -4,7 +4,7 @@ import {View, Text} from 'react-native';
 export default function HomeScreen() {
   return (
     <View>
-      <Text>Esta é a tela inicial</Text>
+      <Text>Esta é a tela iniciala</Text>
     </View>
   );
 }
